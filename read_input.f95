88643e80ff1fd1fe0d67a8180df3823aaa40076a
ashishbhole <ashishbhole07@gmail.com>
2016-06-24 22:42:56 +0530

    updated read_input.dat

--- read_input.f95
+++ read_input.f95
@@ -10,8 +10,6 @@
       read(1,*)
       read(1,'(F4.2,X,I1,X,I1)') version, file_type, data_size
       read(1,*)
-      read(1,'(I1)') nphys
-      read(1,*)
       read(1,'(I1)') ntags
       read(1,*)
       read(1,*) wst, wen
