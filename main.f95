50e4cb12df5effe7368fca34ee3e5093535af33b
ashishbhole <ashishbhole07@gmail.com>
2016-06-24 22:35:05 +0530

    updated main.f95
    named variable for nblocks

--- main.f95
+++ main.f95
@@ -1,3 +1,16 @@
+      module var
+      implicit none
+      integer :: i, j, k, nblocks
+      integer :: imax, jmax, wst, wen
+      integer :: nphys, ntags 
+      integer :: file_type, data_size, nperiodic
+
+      real :: version, z = 0.0
+      double precision, dimension(:,:), allocatable :: x,y
+
+      character (len=40) :: ifilename, ofilename
+      end module var
+
       program converter
       use var
       implicit none
@@ -5,14 +18,14 @@
       call read_input
 
       open(1,file=ifilename)
-      read(1,*) nd
+      read(1,*) nblocks
       read(1,*) imax, jmax
       allocate(x(1:imax, 1:jmax), y(1:imax, 1:jmax))
-      do k=1,nd
+      do k=1,nblocks
          read(1,*) ((x(i,j),i=1,imax),j=1,jmax), ((y(i,j),i=1,imax),j=1,jmax)
       enddo
       close(1)
 
-      call check(x,y)    ! optional: just to check reading of plot3d file
+      call check(x,y)
       call msh_ascii(x,y)
       end program
